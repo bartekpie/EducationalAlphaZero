6b494d1a17ad3803218ab69bcd6b0647c4e876a3
Bartek Pietruczuk <pietruczukbartek@gmail.com>
2026-01-19 17:36:11 +0100

    add TicTacToe class

---
+++ include/TicTacToe.hpp
@@ -0,0 +1,19 @@
+#include "game.hpp"
+#include <array>
+namespace TicTacToe {
+  enum class Player{
+    None, 
+    Cross,
+    Circle
+  };
+
+  using gameState = std::array<std::array<Player, 3>, 3>;
+  
+  class Ttt : public Game {
+    public:
+      Ttt();
+      gameState&& getState()const;
+    private:
+      gameState state_;
+  };
+}
