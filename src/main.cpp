74c0f65d63d4caf52fd7b336f0a5a2ef7847c241
Bartek Pietruczuk <pietruczukbartek@gmail.com>
2026-01-31 13:46:26 +0100

    testing mcts

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,8 @@
 
 int main() {
   TicTacToe::Ttt game());
-  NeuralNetwork net(t.getStateSize(), 2, 100, 0.2);
-  Mcts mcts(new node<TicTacToe::Ttt>(game), net);
-  for (auto a{0}; a < 100; a++) {
-    mcts.run(100);
-    auto dataToLearn = exportToTrain(mcts);
-    net.train(dataToLearn);
-  }
+  NeuralNetwork net(10, 2, 10000, 0.2);
+  Mcts<TicTacToe::Ttt> mcts(net);
+  mcts.run(100);
+  
 }
