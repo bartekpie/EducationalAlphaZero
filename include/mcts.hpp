397f42e914a0265c2f62910986045d4c2d710425
Bartek Pietruczuk <pietruczukbartek@gmail.com>
2026-01-31 12:37:18 +0100

    small adjustments of Mcts

--- include/mcts.hpp
+++ include/mcts.hpp
@@ -1,45 +1,56 @@
+#pragma once
 #include "game.hpp"
 #include "network.hpp"
 #include <iostream>
 #include <memory>
 #include <vector>
 
+// helper template class to structure the node of personalizetd MCTS 
+
 template<Game G>
 class Node {
   public:
-   template<typename T>
-   friend class Mcts;
+    template<typename T> friend class Mcts;
     Node(G game, Node* parent=nullptr, int actionTaken = -1) : 
       state_(game),
-      actionTaken_(actionTaken),
-      parent_(parent) {}
+      parent_(parent),
+      value_(0.0),
+      prior_(0.0),
+      visitCount_(0) {}
   private:
     std::vector<std::unique_ptr<Node<G>>> children_;
     Node* parent_;
     G state_;
-    int vistCount_;
-    int value_;
+    int visitCount_;
+    double value_;
+    double prior_;
 
-}
+
+};
+
 //purpose of this class is to simulate monte-carlo tree search
-//with
+//chenged to be compatible with alpha zero uct -> puct , no rnadom simualtion using nn instead
+
 template<Game G>
 class Mcts {
   public: 
-    friend class AlphaZero;
-    Mcts();
-    Mcts(Node<G>* root, NeuralNetwork net) : net_(net) {};
-    run();
+    Mcts(Node<G>* root = nullptr, NeuralNetwork& net) : 
+      net_(net),
+      root_(root),
+      procentOfKnowledge_(0.0) {};
 
-    
+    void run(int num); 
+
   private:
+    int currBackProp_{0};
+    int termBackProp_{0};
+    double procentOfKnowledge_;
     Node<G>* root_;
     NeuralNetwork& net_;
-    double uct(double policy);
-    Node* select(Node* node);
-    Node* expand(Node* node);
-    backpropagate();
-    iterationMcts();
-    
-
-}
+    double uct();
+    Node* select(Node<G>* node);
+    Node* expand(Node<G>* node);
+    void backpropagade(Node<G>* node, Status status);
+    void backpropagade(Node<G>* node, double value);
+    void iteration();
+};
--- src/mcts.cpp
+++ src/mcts.cpp
@@ -1,55 +1,81 @@
 #include "mcts.hpp"
+#include <cassert>
 
-double Mcts::uct(double policy) {
+template<Game G>
+double Mcts::uct(Node<G>* child) {
     if (child -> visitcount == 0) 
       return std::numeric_limits<double>::infinity();
-    return (child->value / child->visitcount) + policy * std::sqrt(std::log(parent->visitcount) / child->visitcount);
+    return ( child->value_ / child->visitCount_ ) + child->prior_ * std::sqrt(std::log(child->parent_->visitcount_) / child->visitcount_);
 }
-Node* Mcts::select(Node* node) {
-    while (node->state_.isTerminal()) {
+template<Game G>
+Node<G>* Mcts::select(Node<G>* node) {
+    while (node->state_.isTerminal() == false) {
         if (node->children.size() < node->state_.getLegalMoves().size()) {
             return node;
         }
         double best{-1e9};
-        Node* bestNode = nullptr;
+        Node<G>* bestNode = nullptr;
         for (const auto& child : node->children) {
-          auto curr = uct(child, net(child->state_));
+          auto curr = uct(child);
           if (curr>best) {
             best = curr;
             bestNode = child;
           }
-
         }
         node = bestNode;
     }
     return node;
 }
-Node* Mcts::expand(Node* node) {
+template<Game G>
+Node<G>* Mcts::expand(Node<G>* node) {
     const auto& moves = node->state_.getLegalMoves();
-    if (node->children.size() < moves.size()) {
-        auto move = moves[node->children.size()];
-        auto game = node->state_.applyMove(move);
-        node.children.push_back(std::make_unique<Node<G>>(game, node, move));
-        return node->children.back().get();
-     )
-    } else {
-        
-    }
-}
-void Mcts::backpropagade(Node* node, Status status) {
-    while (node->parent != nullptr) {
-       node ->visit 
-    }
-}
-void Mcts::backpropagade(Node* node, int value) {
+    std::assert(node->children.size() < moves.size());
+    auto move = moves[node->children.size()];
+    auto game = node->state_.applyMove(move);
+    node->children.push_back(std::make_unique<Node<G>>(game, node, move));
+    return node->children.back().get();
     
-}        
-void Mcts::mctsiteration(Node* root) {
-    Node* node = select(root);
-    if (!isTerminal(node->gamestate)) {
-      backpropagate(node, node->state_.getStatus());
-    } else{
-       node = expand(node);
-       backpropagate(node, net_.forward(node->state_).result());
+    
+}
+template<Game G>
+void Mcts::backpropagade(Node<G>* node, Status status) {
+    while (node->parent != nullptr) {
+       node->visitCount_++;
+       if(status == Status::player1Win)
+        node->value_ += (node->state_.getCurrentPlayer() == Player::player1) ? 1 : -1;
+       if(status == Status::player2Win)
+        node->value_ += (node->state_.getCurrentPlayer() == Player::player2) ? 1 : -1;
+       node = node->parent_;
     }
+} 
+template<Game G>
+void Mcts::backpropagade(Node<G>* node, double value) {
+    while (node->parent != nullptr) {
+       node->visitCount_++;
+       node->value_ += value;
+       value = -value;
+       node = node->parent_;
+    }
+}  
+template<Game G>      
+void Mcts::iteration() {
+    Node<G>* node = root_;
+    node = select(node);
+    if (isTerminal(node->state_)) {
+      backpropagate(node, node->state_.getGameStatus());
+      termBackProp++;
+    } else {
+       node = expand(node);
+       auto [policy, value] = net_.forward(node->state_);
+       node->prior = policy;
+       backpropagate(node, value);
+    }
+}
+template<Game G>
+void Mcts::run(int num) {
+  for (auto i{0}; i < num; i++){
+    iteration();
+    currBackProp++;
+    procentOfKnowledge_ = (termBackProp / currBackProp);
+  }
 }
